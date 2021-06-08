01d7d949f380b4d418670a7fec908462bbc7ed74
TimmyNic <tim.nicolas@outlook.com>
2021-06-08 16:00:03 +1200

    Updated ConfigureDialog() arguments.

--- mapclientplugins/transformmodeltoimagespacestep/step.py
+++ mapclientplugins/transformmodeltoimagespacestep/step.py
@@ -92,7 +92,7 @@ class TransformModeltoImageSpaceStep(WorkflowStepMountPoint):
         then set:
             self._configured = True
         '''
-        dlg = ConfigureDialog()
+        dlg = ConfigureDialog(self._main_window)
         dlg.identifierOccursCount = self._identifierOccursCount
         dlg.setConfig(self._config)
         dlg.validate()
