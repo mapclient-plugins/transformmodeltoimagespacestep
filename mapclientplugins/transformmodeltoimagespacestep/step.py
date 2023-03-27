8fa1d775e9dc3b946d18c4bc0960412220d21455
Hugh Sorby <h.sorby@auckland.ac.nz>
2023-03-27 15:39:10 +1300

    Tidy documentation.

--- mapclientplugins/transformmodeltoimagespacestep/step.py
+++ mapclientplugins/transformmodeltoimagespacestep/step.py
@@ -1,7 +1,3 @@
-'''
-MAP Client Plugin Step
-'''
-
 import json
 
 from mapclient.mountpoints.workflowstep import WorkflowStepMountPoint
@@ -11,10 +7,6 @@ from gias3.image_analysis import fw_segmentation_tools as fst
 
 
 class TransformModeltoImageSpaceStep(WorkflowStepMountPoint):
-    '''
-    Skeleton step which is intended to be a helpful starting point
-    for new steps.
-    '''
 
     def __init__(self, location):
         super(TransformModeltoImageSpaceStep, self).__init__('Transform Model to Image Space', location)
@@ -31,21 +23,13 @@ class TransformModeltoImageSpaceStep(WorkflowStepMountPoint):
         self.addPort(('http://physiomeproject.org/workflow/1.0/rdf-schema#port',
                       'http://physiomeproject.org/workflow/1.0/rdf-schema#uses',
                       'ju#fieldworkmodeldict'))
-        self._config = {}
-        self._config['identifier'] = ''
-        self._config['Mirror'] = 'False'
-        self._config['Z Shift'] = 'False'
+        self._config = {'identifier': '', 'Mirror': 'False', 'Z Shift': 'False'}
 
         self._scan = None
         self._inputModelDict = None
         self._outputModelDict = None
 
     def execute(self):
-        '''
-        Add your code here that will kick off the execution of the step.
-        Make sure you call the _doneExecution() method when finished.  This method
-        may be connected up to a button in a widget for example.
-        '''
         if self._config['Mirror'] == 'False':
             ns = False
         elif self._config['Mirror'] == 'True':
@@ -63,11 +47,6 @@ class TransformModeltoImageSpaceStep(WorkflowStepMountPoint):
         self._doneExecution()
 
     def setPortData(self, index, dataIn):
-        '''
-        Add your code here that will set the appropriate objects for this step.
-        The index is the index of the port in the port list.  If there is only one
-        uses port for this step then the index can be ignored.
-        '''
         if index == 1:
             if len(dataIn) > 1:
                 raise ValueError('only one image supported.')
@@ -77,21 +56,9 @@ class TransformModeltoImageSpaceStep(WorkflowStepMountPoint):
             self._inputModelDict = dataIn  # ju#fieldworkmodeldict
 
     def getPortData(self, index):
-        '''
-        Add your code here that will return the appropriate objects for this step.
-        The index is the index of the port in the port list.  If there is only one
-        provides port for this step then the index can be ignored.
-        '''
         return self._outputModelDict
 
     def configure(self):
-        '''
-        This function will be called when the configure icon on the step is
-        clicked.  It is appropriate to display a configuration dialog at this
-        time.  If the conditions for the configuration of this step are complete
-        then set:
-            self._configured = True
-        '''
         dlg = ConfigureDialog(self._main_window)
         dlg.identifierOccursCount = self._identifierOccursCount
         dlg.setConfig(self._config)
@@ -105,29 +72,15 @@ class TransformModeltoImageSpaceStep(WorkflowStepMountPoint):
         self._configuredObserver()
 
     def getIdentifier(self):
-        '''
-        The identifier is a string that must be unique within a workflow.
-        '''
         return self._config['identifier']
 
     def setIdentifier(self, identifier):
-        '''
-        The framework will set the identifier for this step when it is loaded.
-        '''
         self._config['identifier'] = identifier
 
     def serialize(self):
-        '''
-        Add code to serialize this step to disk. Returns a json string for
-        mapclient to serialise.
-        '''
         return json.dumps(self._config, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
     def deserialize(self, string):
-        '''
-        Add code to deserialize this step from disk. Parses a json string
-        given by mapclient
-        '''
         self._config.update(json.loads(string))
 
         d = ConfigureDialog()
